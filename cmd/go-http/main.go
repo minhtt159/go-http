4ab42adc7e3e08b68a16771538525386cf3262a1
minhtt159 <4847344+minhtt159@users.noreply.github.com>
2024-10-05 10:54:49 +0200

    change default var and logs

--- cmd/go-http/main.go
+++ cmd/go-http/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,20 +12,21 @@ var (
 	app = kingpin.New(filepath.Base(os.Args[0]), "Simple HTTP Server")
 
 	verbose = app.Flag("verbose", "Verbose level").Short('v').Bool()
-	server  = app.Flag("server", "Listen interface").Short('s').Default("127.0.0.1").IP()
+	server  = app.Flag("server", "Listen interface").Short('s').Default("0.0.0.0").IP()
 	port    = app.Flag("port", "Listen port").Short('p').Default("8080").String()
 
+	// Cmd: go-http serve
 	action = app.Command("serve", "Start in server mode").Default()
 	path   = action.Arg("path", "Listen path").Default("/").String()
 )
 
 func main() {
-	fmt.Println("All Args", os.Args)
+	log.Printf("All Args: %s", os.Args)
 
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 	case action.FullCommand():
-		fmt.Println("Start in serve mode")
-		fmt.Printf("All flags: \nverbose: %t\nserver: %s\nport: %s\npath: %s\n",
+		log.Println("Start in serve mode")
+		log.Printf("All flags: \nverbose: %t\nserver: %s\nport: %s\npath: %s\n",
 			*verbose, *server, *port, *path)
 	default:
 		log.Fatal("Action not valid")
